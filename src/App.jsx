81dfe73b78302ab830f0ba263efd0a26e8814145
EmaniAditya <esaadityareddy@gmail.com>
2025-01-26 07:43:26 +0530

    hmm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 function App() {
   
   return <div>
-
+    <div>
+      <input type="text" defaultValue={"SHRI RAWATPURA SARKAR UNIVERSITY"} placeholder="University Name"/>
+      <input type="text" defaultValue={"RAIPUR (C.G.)"} placeholder="University Place"/>
+    </div>
+    <div>
+      <input type="text" defaultValue={"Department of Computer Science & Engineering"} placeholder="Department Name"/>
+    </div>
+    <div>
+      <input type="image" defaultValue={"upload logo"} placeholder="upload logo"/>
+    </div>
+    <div>
+      <input type="text" defaultValue={"Assignment File"} placeholder="Purpose"/>
+    </div>
+    <div>
+      <input type="text" defaultValue={"On"} placeholder="On"/>
+      <input type="text" defaultValue={"Unix and Shell Programming"} placeholder="Subject Name"/>
+      <input type="text" defaultValue={"(BENCS504T)"} placeholder="Subject Code"/>
+    </div>
+    <div>
+      <input type="text" defaultValue={"For"} placeholder="For"/>
+      <input type="text" defaultValue={"B.Tech.(CSE) - 5th Semester"} placeholder="Branch & Sem"/>
+    </div>
+    <div>
+      <div>
+        <input type="text" defaultValue={"Submitted By:"} placeholder="Submitted By"/>
+        <input type="text" defaultValue={"Name: E S Aaditya Reddy"} placeholder="Name"/>
+        <input type="text" defaultValue={"Enroll No: SRUAC2188"} placeholder="Enroll No"/>
+        <input type="text" defaultValue={"Roll Number: W2302537"} placeholder="Roll Number"/>
+      </div>
+      <div>
+        <input type="text" defaultValue={"Submitted To:"} placeholder="Submitted To"/>
+        <input type="text" defaultValue={"Miss. Payal Kapadia"} placeholder="Name"/>
+        <input type="text" defaultValue={"Assistant Professor"} placeholder="Designation"/>
+      </div>
+    </div>
+    <div>
+      <button onClick={""}>Download PDF</button>
+    </div>
   </div>
 }
 
